8bfc0cece575600036249ea57962f330dcd4dd5c
steferguson <52539323+steferguson@users.noreply.github.com>
2022-11-29 16:25:40 +0000

    log body

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.post('/slack-webhook-verify', (req, res) => {
+  console.log(req.body);
   res.status(200).json({ challenge: req.body.challenge });
 });
 
