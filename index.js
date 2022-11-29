7598f7c2abd2763416e13a886a40aace36460376
steferguson <52539323+steferguson@users.noreply.github.com>
2022-11-29 16:22:03 +0000

    Add slack verify webhook

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.post('/slack-webhook-verify', (req, res) => {
+  res.status(200).json({ challenge: req.body.challenge });
+});
+
+app.post('/slack-webhook', (req, res) => {
+  res.status(200).json({ msg: 'yes' });
+});
+
 app.all('/', (req, res) => {
   console.log('Just got a request!');
+
   res.send('Yo!');
 });
 
