72299089fb58289d6c1747b7821747adf7a54b40
steferguson <52539323+steferguson@users.noreply.github.com>
2022-11-30 14:16:57 +0000

    remove req log

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ app.post('/slack-webhook-verify', (req, res) => {
 });
 
 app.post('/slack-command-register', (req, res) => {
-  console.log(req);
   console.log(req.body);
 
   res.status(200).send('Thank you for registering!');
